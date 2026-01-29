e3058e6b0ab009ea9f53f850af7e74d9fe8dd1ef
Changyao Chen <changyao.chen@instacart.com>
2026-01-29 15:36:10 -0500

    fix for cpu

--- runs/runcpu.sh
+++ runs/runcpu.sh
@@ -4,11 +4,10 @@
 # This script was last updated/tuned on Jan 17, 2026.
 
 # Run as:
-# bash dev/cpu_demo_run.sh
+# bash runs/runcpu.sh
 
 # NOTE: Training LLMs requires GPU compute and $$$. You will not get far on your Macbook.
 # Think of this run as educational/fun demo, not something you should expect to work well.
-# (This is why I hide this script away in dev/)
 # You may also want to run this script manually and one by one, copy pasting commands into your terminal.
 
 # all the setup stuff
